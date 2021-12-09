7d31069dad1be687e92e256e4e73bf01577640fc
Julie SUN <sun.julie0@gmail.com>
2021-12-09 16:50:55 +0100

    ajout fichiers

--- modeles/modelArticle.php
+++ modeles/modelArticle.php
@@ -13,8 +13,13 @@ function nouvelArticle() {
         $libelle = filter_input(INPUT_POST,'libelle');
         $prix = filter_input(INPUT_POST,'prix');
         $photo = $_FILES['photo']['name'];
-        $param = explode('.', $photo);
-        $photo = uniqid() . '.' .array_pop($param); 
+        // $param = explode('.', $photo);
+        // $photo = uniqid() . '.' .array_pop($param); 
+
+        $param = pathinfo($photo); //fonction retournant infos sur un fichier
+        //var_dump($param); echo '<br>';
+        $photo = uniqid() . '.' .$param['extension'];
+        //var_dump($photo);die();
 
         ajoutArticle($libelle, $photo, $prix);
 
